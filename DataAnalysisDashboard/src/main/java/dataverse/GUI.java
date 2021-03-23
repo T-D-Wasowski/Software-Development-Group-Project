2a92ebd7e56a708f0b82c08e1c44457c1463e35f
Isa <isa2qamber@gmail.com>
2021-03-23 20:23:20 +0300

    GUI creation

--- DataAnalysisDashboard/src/main/java/dataverse/GUI.java
+++ DataAnalysisDashboard/src/main/java/dataverse/GUI.java
@@ -1,10 +1,28 @@
 package dataverse;
+import javax . swing .*;
+import java . awt . event .*;
 
 public class GUI {
     
-    public void login() {
-             
-        
+    class MyWindowListener extends WindowAdapter {
+        public void windowClosing(WindowEvent e) {
+            System.out.println("Closing window.");
+            System.exit(0);
+        }
     }
+    
+    
+    public void login() {
+       JFrame window = new JFrame("LoginWindow");
+       window.addWindowListener(new MyWindowListener());
+       window.setSize(400,400);
+       window.setVisible(true);
+       
+       
+    }
+    
+    public void homePage() {
+    
+}
     
 }
