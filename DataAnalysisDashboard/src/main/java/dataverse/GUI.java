d5427d6bd0fc41e1865de5ce384d34c851a0c636
Kazaius <ContactYas382@gmail.com>
2021-03-28 03:26:32 +0100

    Added Drag and Drop HomePage GUI (In Progress)

--- DataAnalysisDashboard/src/main/java/dataverse/GUI.java
+++ DataAnalysisDashboard/src/main/java/dataverse/GUI.java
@@ -78,21 +78,24 @@ public class GUI {
    
     
     public void homePage() {
-    JFrame HWind = new JFrame (" Home Page " );
-       HWind.setTitle("Home Page");
-       HWind.addWindowListener(new MyWindowListener());
-       HWind.setSize(400,400); //Setting size of frame
-       HWind.setVisible(true); //Ensuring that frame is visible
-    
-    JPanel ExperimentalB = new JPanel(new FlowLayout(FlowLayout.CENTER));
-    ExperimentalB.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-    
-    JButton Filter1 = new JButton("Register");
-       Filter1.setBounds(130,100,100,40);
-       
-       ExperimentalB.add(Filter1); 
-       
-    
+        JFrame frame = new JFrame("Home Page");
+           frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+           frame.setSize(600,600);
+        
+           //Creating the MenuBar and adding components
+        JMenuBar mb = new JMenuBar();
+        JMenu m1 = new JMenu("FILE");
+        mb.add(m1);
+        JMenuItem m11 = new JMenuItem("Confirm Logout");
+        m1.add(m11);
+
+        // Text Area at the Center
+        JTextArea ta = new JTextArea();
+
+        //Adding Components to the frame.
+        frame.getContentPane().add(BorderLayout.NORTH, mb);
+        frame.getContentPane().add(BorderLayout.CENTER, ta);
+        frame.setVisible(true);
 }
     
 }
